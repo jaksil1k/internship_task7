0e476ecc653d61c7f5d74f02853749a7e38e5848
Zhaksylyk Omirbekov <nartai53@gmail.com>
2023-03-16 10:40:55 +0600

    created db

---
+++ src/main/java/com/example/task7/Task7Application.java
@@ -0,0 +1,22 @@
+package com.example.task7;
+
+import com.example.task7.util.DBTables;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class Task7Application {
+
+    public static void main(String[] args) {
+        try {
+            DBTables.createTables();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        SpringApplication.run(Task7Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/task7/util/DBTables.java
@@ -0,0 +1,53 @@
+package com.example.task7.util;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public final class DBTables {
+
+
+    private static final String METERS ="""
+                CREATE TABLE IF NOT EXISTS meters(
+                meter_id SERIAL PRIMARY KEY,
+                meter_type VARCHAR(255) NOT NULL,
+                meter_group VARCHAR(255) NOT NULL,
+                meter_timestamp TIMESTAMP NOT NULL,
+                current_reading double precision NOT NULL,
+                CONSTRAINT fk_meter_group
+                FOREIGN KEY(meter_group)
+                REFERENCES meter_groups(name)
+                );
+                """;
+    private static final String METER_GROUPS = """
+                CREATE TABLE IF NOT EXISTS meter_groups(
+                name VARCHAR(255) PRIMARY KEY
+                )
+                """;
+
+    private static final String READINGS = """
+                CREATE TABLE IF NOT EXISTS to_do_list(
+                id SERIAL PRIMARY KEY,
+                meter_id SERIAL NOT NULL,
+                reading double precision NOT NULL,
+                CONSTRAINT fk_meter
+                FOREIGN KEY(meter_id)
+                REFERENCES meters(meter_id)
+                )
+                """;
+
+
+
+    public static void createTables() throws SQLException {
+        try (Connection connection = ConnectionManager.get();
+             Statement statement = connection.createStatement()) {
+            statement.execute(METER_GROUPS);
+            statement.execute(METERS);
+            statement.execute(READINGS);
+            System.out.println("Database successfully started");
+        }
+    }
+}
