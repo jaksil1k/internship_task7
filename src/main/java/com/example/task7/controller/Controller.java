6a4cd0db6a2ee7b2b01eb660e7fc61e1ef1d55c8
Zhaksylyk Omirbekov <nartai53@gmail.com>
2023-04-01 17:31:22 +0600

    added user authentication and authentication API with jwt token

--- src/main/java/com/example/task7/controller/Controller.java
+++ src/main/java/com/example/task7/controller/Controller.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1.0/meters")
+@RequestMapping("/api/v1/meters")
 public class Controller {
 
     private final MeterService meterService;
---
+++ src/main/java/com/example/task7/dao/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.task7.dao;
+
+
+import com.example.task7.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+}
+
